002e6dfae269a2276c2350476647c658bfffbf96
John Zuccarelli <johnzuccarelli1@gmail.com>
2026-09-24 11:44:40 -0400

    split handlers and routes into separate files and add app struct

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -9,32 +9,38 @@ import (
 	"github.com/joho/godotenv"
 )
 
-func main() {
-	slog.Info("booting archie api...")
+type application struct {
+	logger *slog.Logger
+}
 
-	if err := godotenv.Load(); err != nil {
-		slog.Debug("no .env file loaded", "error", err)
+func main() {
+
+	app := &application{
+		logger: slog.New(slog.NewJSONHandler(os.Stdout, nil)),
 	}
 
-	mux := http.NewServeMux()
+	if err := godotenv.Load(); err != nil {
+		app.logger.Debug("no .env file loaded", "error", err)
+	}
+
 	port := os.Getenv("PORT")
 
 	if port == "" {
 		port = "4000"
 	}
 
-	slog.Info("starting server on", "port", port)
+	app.logger.Info("starting server on", "port", port)
 
 	srv := &http.Server{
 		Addr:         ":" + port,
-		Handler:      mux,
+		Handler:      app.routes(),
 		ReadTimeout:  5 * time.Second,
 		WriteTimeout: 10 * time.Second,
 		IdleTimeout:  120 * time.Second,
 	}
 
 	if err := srv.ListenAndServe(); err != nil {
-		slog.Error("Error starting server...", "error", err)
+		app.logger.Error("Error starting server...", "error", err)
 		os.Exit(1)
 	}
 
